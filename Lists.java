77454fcc0790bb1ad08580017d2c9254c68d8ca3
Syreldar <enrico.drago3@gmail.com>
2023-07-11 14:11:37 +0200

    Upload exercise

--- Lists.java
+++ Lists.java
@@ -1,6 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Lists
 {
     public static void main(String[] args)
     {
+        List<String> cityNames = Arrays.asList("New York", "London", "Tokyo");
+        //cityNames.add("Berlin"); // UnsupportedOperationException: The cityNames list is immutable.
+        cityNames.set(1, "Tegucigalpa");
+
+        System.out.println(cityNames);
+
+        List<String> kingsOfRome = new ArrayList<>(
+                Arrays.asList(
+                        "Romulus",
+                        "Numa Pompilius",
+                        "Tullus Hostilius",
+                        "Ancus Marcius",
+                        "Lucius Tarquinius Priscus",
+                        "Servius Tullius",
+                        "Lucius Tarquinius Superbus"
+                )
+        );
+        System.out.println(kingsOfRome);
+
+        String[] kingsOfRomeArray = kingsOfRome.toArray(new String[0]);
+        kingsOfRomeArray[6] = "Lucius Tarquinius Supercar";
+
+        System.out.println(Arrays.toString(kingsOfRomeArray));
     }
 }
